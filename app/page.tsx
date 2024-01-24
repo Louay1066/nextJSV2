09ade4abbb99fbc9160694c3db44ceb7f6967fd7
Louay1066 <louayabrame4@hotmail.com>
2024-01-24 12:11:05 +0300

    removed the page

--- app/page.tsx
+++ app/page.tsx
@@ -20,13 +20,7 @@ export default function Page() {
 />
           <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
       {/* Add Hero Images Here */}
-      <Image
-        src="/hero-desktop.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
+      
     </div>
           <p className={`${lusitana.className}text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
@@ -55,7 +49,7 @@ export default function Page() {
         width={560}
         height={620}
         className=" block md:hidden"
-        alt="Screenshots of the dashboard project showing desktop version"
+        alt="Screenshots of the dashboard project showing mobile version"
       />
         </div>
       </div>
